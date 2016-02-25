5e96f77636c4a83ee7caaf9844f26fba8945ac36
shoab10 <shoab10@gmail.com>
2016-02-25 13:20:18 -0600

    Context and Bean params

--- src/main/java/org/shoab/messenger/resources/InjectDemoResource.java
+++ src/main/java/org/shoab/messenger/resources/InjectDemoResource.java
@@ -7,7 +7,10 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/injectdemo")
 @Consumes(MediaType.TEXT_PLAIN)
@@ -15,10 +18,18 @@ import javax.ws.rs.core.MediaType;
 public class InjectDemoResource {
 
 	@GET
-	@Path("/annotations")
+	@Path("annotations")
 	public String getParamsUsingAnnotations(@MatrixParam("param") String param,
 											@HeaderParam("customHeaderValue") String header,
 											@CookieParam("name") String cookie) {
 		return "Matrix param : " + param + " Header Param: "+ header + " Cookie Param: "+ cookie;
+	}
+	
+	@GET
+	@Path("context")
+	public String getParamsUsingContext(@Context UriInfo uriInfo, @Context HttpHeaders headers ){
+		String path = uriInfo.getAbsolutePath().toString();
+		String cookies = headers.getCookies().toString();
+		return "Path: "+ path + " Cookies: " + cookies;
 	}
 }
